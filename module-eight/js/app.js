05c73c93c93d9255ffc9b5ac51e3ff631ff0e2f7
unknown <jem28santos@yahoo.com>
2013-09-13 02:39:13 +0800

    update

--- module-eight/js/app.js
+++ module-eight/js/app.js
@@ -1,31 +1,34 @@
-<<<<<<< HEAD
-$(function(){
-	$('#movie_text').keypress(function(event) {
-		
-		var keycode = (event.keyCode ? event.keyCode : event.which);
-		if(keycode == '13') {
-			var movie_title =  $("#movie_text").val();
-				if (txtTitle.value == ""){
-					alert("Please insert a Movie Title.");
-				}
-				else{
-					alert(movie_title);
-				}
-			$('#movie_text').val('');
-		}
-	});
-});
-
-	$("#boxOffice").click(function(){
-		//sendRequest('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?');
-		alert("box_office");
-	});
-=======
 $(document).ready(function() {
     var app = {};
     var url = '';
->>>>>>> b4ecc663bd4712a1c5ef602db05a3278245a7619
-	
+
+    $('#movie-text').keypress(function(event) {
+		var keycode = (event.keyCode ? event.keyCode : event.which);
+		if(keycode == '13') {
+			var movie_title =  $("#movie-text").val();
+			searchMovies(movie_title);
+				if (movie_title.value == ""){
+					alert("Please insert a Movie Title.");
+				}
+				else{
+
+					$.ajax({
+					url: 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?',
+					dataType: 'jsonp',
+					data:{
+						q: movie_title,
+						apiKey: 'hcrurhsttexasrgfm2y6yahm', page_limit: 50, page: 1
+					},
+					success:showMovies
+
+					});
+					$('#movie-text').val('');
+
+				}
+
+		}
+	});
+
 	url = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?';
 	sendRequest(url);
 
@@ -33,23 +36,20 @@ $(document).ready(function() {
 		url = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?';
 		sendRequest(url);
 	});
-		
+
 	$('#in_theaters').click(function(){
 		url = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?';
 		sendRequest(url);
-		//alert(url);
 	});
 
 	$('#up_coming').click(function(){
 		url = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json?';
 		sendRequest(url);
-		//alert(url);
 	});
 
 	$('#opening_movies').click(function(){
 		url = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/opening.json?';
 		sendRequest(url);
-		//alert(url);
 	});
 
  	function sendRequest(url){
